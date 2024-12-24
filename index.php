d85689f352e59e4a34c217dd83c0a945b85962eb
Raza Haan <razahaanfiddo@gmail.com>
2024-12-24 10:22:30 +0700

    menambahkan fitur daftar

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
             <ul class="navbar">
                 <li><a href="#home">Home</a></li>
                 <li><a href="pemesanan.php">Pemesanan</a></li>
+                <li><a href="daftar.php">Daftar</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#destinasi">Destinasi</a></li>
                 <li><a href="#gallery">Gallery</a></li>
