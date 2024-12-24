939cd0e0d74a68ee646a8c034b96d5876ae9594f
Raza Haan <razahaanfiddo@gmail.com>
2024-12-24 10:38:49 +0700

    memberi stye pada fitur daftar

--- daftar.php
+++ daftar.php
@@ -1,4 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MajVel - Wisata Majalengka</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sticky-footer-navbar/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <!-- Header -->
+    <header>
+        <a href="#" class="logo">MajVel</a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+            <ul class="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="daftar.php">Daftar</a></li>
+            </ul>
+    </header>   
+
+    <!-- Home Section -->
+    <section class="home" id="home" style="background-image: url(asset/img/home.jpg);">
+        <div class="home-text">
+            <h1>Imajinasimu Adalah <br> Satu-satunya Batasanmu</h1>
+        </div>
+    </section>
+
+    <?php
 include 'lib/koneksi.php';
 
 $sql = "SELECT * FROM pemesanan where is_deleted = 0 order by created_at desc";
@@ -11,8 +42,8 @@ if(mysqli_num_rows($query)==0)
 }else{
     $detail = mysqli_fetch_row($query);
 ?>
-<h1>Daftar Pemesanan</h1>
-<table class="table">
+<h1 class="text-center my-4">Daftar Pemesanan</h1>
+<table class="table container">
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -45,3 +76,10 @@ if(mysqli_num_rows($query)==0)
 <?php
 } 
 ?>
+<section>
+    <br><br><br><br><br><br>
+</section>
+    <!-- link to js -->
+    <script type="text/javascript" src="asset/js/script.js"></script>
+</body>
+</html>
--- lib/koneksi.php
+++ lib/koneksi.php
@@ -7,10 +7,10 @@ $database_name = 'majvel';
 
 $db = mysqli_connect($hostname, $username, $password, $database_name);
 
-if($db->connect_error){
-    echo 'gagal';
-}else{
-    echo 'berhasil';
-}
+// if($db->connect_error){
+//     echo 'gagal';
+// }else{
+//     echo 'berhasil';
+// }
 
 ?>
